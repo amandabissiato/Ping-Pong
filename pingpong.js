b5b00a029d75e19b3a78890aeb238d92e1dca1b0
amandabissiato <amanda.bissiato@escola.pr.gov.br>
2026-02-27 07:55:46 -0300

    Update pingpong.js

--- pingpong.js
+++ pingpong.js
@@ -20,12 +20,40 @@ let yraqueteOponente = 150;
 let meusPontos = 0;
 let pontosOponente = 0;
 
+//dificuldade
+
+let dificuldade = "Normal"
+let trilha,ponto,raquetada;
+
+function preload(){
+  trilha = loadSound("trilha.mp3")
+  ponto = loadSound("ponto.mp3")
+  raquetada = loadSound("raquetada.mp3")
+}
+
 function setup(){
    createCanvas(600,400)
+   trilha.loop()
 }
 
 function draw(){
+  background(0);
+  movimentoBolinha();
+  raquetes(xRaquete,yRaquete);
+  raquetes(xRaqueteOponente,yRaqueteOponente);
+  npc(dificuldade);
   
+  if (keyIsDown(UP_ARROW)){
+    yRaquete -= 10}
+  
+  if(KeyIsDown(DOWN_ARROW)){
+    yRaquete += 10}
+  
+  if(xBolinha -raio < xRaquete + 10 & 
+     yBolinha-raio< yRaquete + 90 & 
+     yBolinha +raio > yRaquete ){
+    velocidadeXBolinha *= -1;
+    raquetada.play()}
 }
 
 function movimentoBolinha(){
