20baa754aaae0b071f470e03623a37ca97f191d6
amandabissiato <amanda.bissiato@escola.pr.gov.br>
2026-03-03 11:20:27 -0300

    update

--- pingpong.js
+++ pingpong.js
@@ -22,13 +22,13 @@ let pontosOponente = 0;
 
 //dificuldade
 
-let dificuldade = "Normal"
+let dificuldade = "Normal";
 let trilha,ponto,raquetada;
 
 function preload(){
-  trilha = loadSound("trilha.mp3")
-  ponto = loadSound("ponto.mp3")
-  raquetada = loadSound("raquetada.mp3")
+  trilha = loadSound("trilha.mp3");
+  ponto = loadSound("ponto.mp3");
+  raquetada = loadSound("raquetada.mp3");
 }
 
 function setup(){
@@ -54,11 +54,33 @@ function draw(){
      yBolinha +raio > yRaquete ){
     velocidadeXBolinha *= -1;
     raquetada.play()}
+  
   if(xBolinha + raio > xRaqueteOponente &
      yBolinha - raio < yRaqueteOponente + 90 &
      yBolinha + raio > yRaqueteOponente) {
   velocidadeXBolinha *= -1;
 raquetada.play()}
+  
+  textSize(18)
+  textAlign(CENTER)
+  fill(color(255,0,233))
+  rect(150,10,40,20)
+  fill(255);
+  text(meusPontos,170,26);
+  fill(color(250,0,233));
+  rect(450,10,40,20);
+  fill(255);
+  text(pontosOponente,470,26);
+  
+  if(xBolinha>595){
+    meusPontos += 1;
+    ponto.play()
+  }
+  if(xBolinha< 10){
+    pontosOponente +=1;
+    ponto.play()
+  }
+  
 }
 
 function movimentoBolinha(){
@@ -68,11 +90,25 @@ function movimentoBolinha(){
    yBolinha += velocidadeYBolinha;
    if(xBolinha + raio > width || xBolinha-raio < 0){
        velocidadeXBolinha *= -1;
+     
    }
    if(yBolinha + raio > height || yBolinha-raio < 0){
-       velocidadeYBolinha *= -1}}
+       velocidadeYBolinha *= -1}
+}
 
 function raquetes(xRaquete,yRaquete){
    rect(xRaquete,yRaquete,10,90)
   
+}
+
+function npc(){
+  var nivel =30;
+  if(dificuldade ==="Fácil"){
+    nivel = 100;
+  }
+   if(dificuldade ==="Normal"){
+   nivel = 30
+  }
+   let velocidadeYRaqueteOponente = yBolinha - yRaqueteOponente - 10 /2;
+  yRaqueteOponente += velocidadeYRaqueteOponente - nivel;
 }
